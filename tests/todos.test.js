8080969e17e29eceeb74a49606b9cc8acb084419
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-03-13 16:04:18 +0000

    ticket 5

--- tests/todos.test.js
+++ tests/todos.test.js
@@ -1,3 +1,10 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
-test("test should run OK", () => {});
+test("practice test should pass", () => {});
+
+test("has title", async ({ page }) => {
+	await page.goto("http://localhost:3000/");
+
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Todo List/);
+});
